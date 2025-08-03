30760978e529198076bb32a407a19c424f69b029
Mayur K Setty <settymayurk@gmail.com>
2025-08-03 18:12:57 +0530

    added protected route for stats page

--- Client/src/pages/Settings.jsx
+++ Client/src/pages/Settings.jsx
@@ -1,63 +1,73 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 import { useUserProfile } from "@/contexts/UserProfileContext";
-import Sidebar from "@/components/settings/Sidebar";
-import BasicInfo from "@/components/settings/BasicInfo";
-import Account from "@/components/settings/Account";
-import Friends from "@/components/settings/Friends";
-import Themes from "@/components/settings/Themes";
-import EducationAndSkills from "@/components/settings/EducationAndSkills";
-import TimeLanguage from "@/components/settings/TimeLanguage";
 import NotLogedInPage from "@/components/NotLogedInPage";
+import ProfileCard from "../components/stats/ProfileCard";
+import MonthlyLevel from "../components/stats/MonthlyLevel";
+import Achievements from "../components/stats/Achievements";
+import Badges from "../components/stats/Badges";
+import StudyStats from "../components/stats/StudyStats";
+import Goals from "../components/stats/Goals";
+import Leaderboard from "../components/stats/Leaderboard";
+import Test from "../components/stats/Test.jsx";
+import AdCard from "@/components/AdCard";
 
-const Settings = () => {
+const Stats = () => {
   const { user, fetchUserDetails } = useUserProfile();
-  const [activeTab, setActiveTab] = useState("basic-info");
-
+  
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       try {
         const decoded = jwtDecode(token);
         fetchUserDetails(decoded.id);
-      } catch (error) {
-        console.error("Error decoding token:", error);
+      } catch (err) {
+        console.error("Error decoding token:", err);
       }
     }
   }, []);
 
-  const renderActiveTab = () => {
-    const protectedTabs = ["basic-info", "Edu-&-skills", "account", "friends"];
-    if (!user && protectedTabs.includes(activeTab)) {
-      return <NotLogedInPage />;
-    }
-
-    switch (activeTab) {
-      case "basic-info":
-        return <BasicInfo />;
-      case "Edu-&-skills":
-        return <EducationAndSkills />;
-      case "account":
-        return <Account />;
-      case "friends":
-        return <Friends />;
-      case "themes":
-        return <Themes />;
-      case "time-language":
-        return <TimeLanguage />;
-      default:
-        return null;
-    }
-  };
+  if (!user) return <NotLogedInPage />;
 
   return (
-    <div className="flex w-[100vw-70px] overflow-hidden m-auto">
-      <Sidebar activeTab={activeTab} onTabChange={setActiveTab} user={user} />
-      <main className="p-6 py-10 bg-primary w-full h-screen overflow-y-auto">
-        {renderActiveTab()}
-      </main>
+    <div className="m-6">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Analytics</h1>
+        <div className="flex items-center gap-4">
+          <span className="text-gray-400">Great! Keep it up</span>
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row gap-6 w-full content-center">
+        <div className="lg:w-[20%] min-w-72 space-y-6">
+          <ProfileCard />
+          <div className="h-auto w-full rounded-3xl bg-sec p-5 space-y-4">
+            <AdCard />
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="mb-6">
+            <StudyStats />
+          </div>
+          <div className="flex flex-col xl:flex-col 2xl:flex-row">
+            <div className="flex-1 mr-6">
+              <div className="flex gap-6 mb-6">
+                <MonthlyLevel />
+                <Badges />
+              </div>
+              <div className=" gap-6 mb-6 ">
+                <Goals />
+              </div>
+            </div>
+            <div>
+              <Achievements />
+              <Leaderboard />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Test />
     </div>
   );
 };
 
-export default Settings;
+export default Stats;
