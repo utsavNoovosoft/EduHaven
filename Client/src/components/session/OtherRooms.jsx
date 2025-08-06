889145d1fcb96afd45da757123ada7526cb064eb
Ashish1022 <ashishjadhav9900@gmail.com>
2025-08-06 21:17:23 +0530

    Small bug fix in OtherRooms page

--- Client/src/components/session/OtherRooms.jsx
+++ Client/src/components/session/OtherRooms.jsx
@@ -114,7 +114,7 @@ export default function OtherRoom({ otherRooms }) {
         </div>
       </div>
 
-      {!isLoading ? (
+      {isLoading ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
           {Array.from({ length: 6 }).map((_, index) => (
             <RoomCardSkeleton key={index} showCategory={true} />
