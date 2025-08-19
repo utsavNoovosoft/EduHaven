89d36caafd7ccc417e0ba88de667693da5202ab2
Aman Sharma <135960284+amandollar@users.noreply.github.com>
2025-08-19 15:57:38 +0530

    Update Db.js

--- Server/Database/Db.js
+++ Server/Database/Db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const ConnectDB = async () => {
     const uri = process.env.MONGODB_URI;
    try {
-    await mongoose.connect(uri, );
+    await mongoose.connect(uri);
     console.log("Connected to Database");
    } catch (error) {
     console.log("Error: ", error.message);
