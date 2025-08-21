3e8bf5bdadd104061ee0f56b2b9d3b5e930ec9c3
kanishka-commits <kanishka03p@gmail.com>
2025-08-21 12:09:11 +0530

    Working Back Button

--- Client/src/components/games/sudoku/Sudoku.jsx
+++ Client/src/components/games/sudoku/Sudoku.jsx
@@ -155,7 +155,9 @@ export default function Sudoku() {
       <nav className={styles.gameHeader}>
         <div className={styles.topBar}>
           <div className={styles.navSection}>
-            <button onClick={() => navigate(-1)} className={styles.iconButton} aria-label="Back">
+            <button
+              onClick={() => window.history.back()} 
+              className={styles.iconButton} aria-label="Back">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
               </svg>
