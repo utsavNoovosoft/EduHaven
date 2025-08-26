0c90a6b794d00e487a2323ea341d099f09c14c6f
Divesh Saini <diveshsaini1991@gmail.com>
2025-08-26 16:52:43 +0530

    Fix: fix logout #516

--- Client/src/Auth/Signout.jsx
+++ Client/src/Auth/Signout.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import axiosInstance from "@/utils/axios";
 import { toast } from "react-toastify";
+const backendUrl = import.meta.env.VITE_API_URL;
+
 
 const Signout = () => {
   const navigate = useNavigate();
