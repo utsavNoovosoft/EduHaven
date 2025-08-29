7f337b09c8879ed5243efbff786d7359a0cdeb5e
Harshawardhan <kambleharsh08@outlook.com>
2025-08-30 02:09:32 +0530

    #494 issue fixed forgot password feature

--- Server/Routes/AuthRoutes.js
+++ Server/Routes/AuthRoutes.js
@@ -4,15 +4,18 @@ import {
   googleAuth,
   googleCallback,
   login,
+  forgotPassword,
+  resetPassword,
   logout,
   refreshAccessToken,
   signup,
   verifyUser,
+  verifyResetOTP
 } from "../Controller/AuthController.js";
 
 const router = express.Router();
 
-// OAuth routes 
+// OAuth routes
 router.get("/google", googleAuth);
 router.get("/google/callback", googleCallback);
 
@@ -20,6 +23,9 @@ router.get("/google/callback", googleCallback);
 router.post("/signup", signup);
 router.post("/verify", verifyUser);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-reset-otp", verifyResetOTP);
+router.post("/reset-password", resetPassword);
 router.post("/logout", logout);
 router.post("/refresh", refreshAccessToken);
 router.post("/delete", deleteAccount);
