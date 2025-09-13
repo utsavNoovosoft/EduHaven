f489a605f9c316cb1d037f716d149040b52c3268
utsav-mehta-noovosoft <utsav.mehta@noovosoft.com>
2025-09-13 20:32:44 +0530

    Refactor socket config

---
+++ Server/Config/socketConfig.js
@@ -0,0 +1,18 @@
+import { Server } from "socket.io";
+import { CORS_ORIGIN } from "./envConfig.js";
+import { initializeSocket } from "../Socket/socket.js";
+
+export function createSocket(server) {
+  return new Server(server, {
+    cors: {
+      origin: CORS_ORIGIN,
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+    pingTimeout: 60000,
+  });
+}
+
+export function initSocketHandlers(io) {
+  initializeSocket(io);
+}
